b1508b1e36d80a320e6a25db4419338a02004c8a
William Writer <williamwriter@Williams-MacBook-Pro-2.local>
2021-11-05 09:54:19 -0600

    the Set method

--- ArrayList.java
+++ ArrayList.java
@@ -1,17 +1,26 @@
 public class ArrayList<T> {
-        private Node root;
-        private int length;
+    private Node root;
+    private int length;
 
-        public ArrayList(){
-            root=new Node();
-            length = 1;
-        }
-        public T set(int i, T data){
-            T prev = new T.get(i);
-            T newValue = new T.get(i);
-            newValue = data;
-            return prev;
-        }
+    public ArrayList(){
+        root=new Node();
+        length = 1;
     }
+    public T set(int i, T data){
+        T prev = new T.get(i);
+        T newValue = new T.get(i);
+        newValue = data;
+        return prev;
+    }
+    public int size(){
+        return this.length;
+    }
+}
+
+
+
+
+
+
 
 
