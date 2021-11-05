66dd495dc74382a66d754ec93c31d8262c7999fd
William Writer <williamwriter@Williams-MacBook-Pro-2.local>
2021-11-05 09:40:59 -0600

    Added size method

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -6,32 +6,11 @@ public class ArrayList<T> {
         root=new Node();
         length = 1;
     }
-    public boolean add(T data){
-        if (length==1){//if it is the second value to be added, then it adds it in special way
-            Node n = new Node(data, root, null);
-            root.set_next(n);
-
-            } else {
-            Node n = new Node(data,recur(root), null);//sets the previous node to the last node on the end of the line
-            recurPrev(root).set_next(recur(root));//sets the old last node to have a next node value of the new node n
-        }
-        return true;
-    }
-    private Node<T> recur(Node<T> n2){//returns the last node in the chain
-        if(n2.getNext()==null){//checks if the current node is the last in the chain and if it does it returns the node
-    return n2;
-        }
-        recur(n2.getNext());//runs the method again on the child node of the previous node
-        return null;
+    public T set(int i, T data){
+        T prev = new T.get(i);
+        T newValue = new T.get(i);
+        newValue = data;
+        return prev;
 
     }
-    private Node<T> recurPrev(Node<T> n2){//returns the second to last node using similar process to the previous one. 
-        if(n2.getNext()==null){
-            return n2.getPrev();
-        }
-        recur(n2.getNext());
-        return null;
-
-    }
-
 }
