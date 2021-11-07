8c3684e76c0591c33108f6bbade3113a154c2fd9
William Writer <williamwriter@Williams-MacBook-Pro-2.local>
2021-11-07 16:48:33 -0700

    The size method

--- ArrayList.java
+++ ArrayList.java
@@ -1,5 +1,19 @@
-public class ArrayList {
+public class ArrayList<T> {
+    private Node root;
+    private int length;
 
     public ArrayList(){
+        root=new Node();
+        length = 1;
+    }
+    public T set(int i, T data){
+        T prev = new T.get(i);
+        T newValue = new T.get(i);
+        newValue = data;
+        return prev;
+    }
+    public int size(){
+        return this.length;
     }
 }
+
